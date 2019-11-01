3b895e9c681eac0cd66590cd017d7f06d42351da
David Malinge <david.malinge@gmail.com>
2019-11-02 00:59:38 +0100

    switch to while loops

--- src/floyd.rs
+++ src/floyd.rs
@@ -4,16 +4,11 @@ use std::fmt;
 struct Racer {
     pub pos: usize,
     pub val: u64,
-    speed: u8,
 }
 
 impl fmt::Debug for Racer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Racer {{ pos: {}, val: {} , speed: {} }}",
-            self.pos, self.val, self.speed
-        )
+        write!(f, "Racer {{ pos: {}, val: {} }}", self.pos, self.val)
     }
 }
 
@@ -24,46 +19,38 @@ impl PartialEq for Racer {
 }
 
 pub fn floyd_cycle_entry_detection(vector: &Vec<u64>) -> (u64, usize, usize) {
-    // Root is at the end of the
+    // Root is at the end of the vector
     let root = vector.len() - 1;
     let tortoise = &mut Racer {
         pos: root,
         val: vector[root],
-        speed: 1,
     };
     let hare = &mut Racer {
         pos: root,
         val: vector[root],
-        speed: 2,
     };
 
-    // dbg!(&tortoise, &hare);
-    vec_jump(&vector, tortoise, tortoise.speed);
-    vec_jump(&vector, hare, hare.speed);
+    vec_jump(&vector, tortoise, 1);
+    vec_jump(&vector, hare, 2);
 
-    loop {
+    while tortoise != hare {
+        vec_jump(&vector, tortoise, 1);
+        vec_jump(&vector, hare, 2);
+    } // Tortoise and hare meet in the cycle
+
+    // Tortoise and hare meet!
+    // We now detected a cycle
+    // This meeting point and the root are both at the same distance to the cycle entry point
+    // Therefore we reset the tortoise and slow down the hare
+    // such that they meet again at the cycle entry point
+    tortoise.pos = root;
+    tortoise.val = vector[root];
+
+    while tortoise != hare {
         // dbg!(&tortoise, &hare);
-
-        if tortoise == hare {
-            // Tortoise and hare meet!
-            if tortoise.speed != hare.speed {
-                // We now detected a cycle
-                // This meeting point and the root are both at the same distance to the cycle entry point
-                // Therefore we reset the tortoise and slow down the hare
-                // such that they meet again at the cycle entry point
-                tortoise.pos = root;
-                tortoise.val = vector[root];
-                hare.speed = 1;
-                continue;
-            } else {
-                // We reached the cycle entry point
-                break;
-            }
-        }
-
-        vec_jump(&vector, tortoise, tortoise.speed);
-        vec_jump(&vector, hare, hare.speed);
-    }
+        vec_jump(&vector, tortoise, 1);
+        vec_jump(&vector, hare, 1);
+    } // Tortoise and hare meet at the entry point
 
     return (
         tortoise.val,
