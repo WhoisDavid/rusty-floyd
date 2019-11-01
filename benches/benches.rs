efe6398839ac8e2c494344ff56fd6cecfefbf868
David Malinge <david.malinge@gmail.com>
2019-11-02 00:58:03 +0100

    fix bench vector

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,21 +5,21 @@ use rusty_floyd::floyd;
 use rusty_floyd::random_vector;
 use test::Bencher;
 
-const size: u64 = 1_000_000;
-const upperbound: Option<u64> = None;
+const SIZE: u64 = 1_000_000;
+const UPPERBOUND: Option<u64> = None;
 
 #[bench]
 fn random_vec_unif(b: &mut Bencher) {
-    b.iter(|| random_vector::random_vector_with_duplicates_unif(size, upperbound));
+    b.iter(|| random_vector::random_vector_with_duplicates_unif(SIZE, UPPERBOUND));
 }
 
 #[bench]
 fn random_vec_gen(b: &mut Bencher) {
-    b.iter(|| random_vector::random_vector_with_duplicates(size, upperbound));
+    b.iter(|| random_vector::random_vector_with_duplicates(SIZE, UPPERBOUND));
 }
 
 #[bench]
 fn floyd_cycle_entry(b: &mut Bencher) {
-    let vector = random_vector::random_vector_with_duplicates(size, upperbound);
+    let vector = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1];
     b.iter(|| floyd::floyd_cycle_entry_detection(&vector));
 }
